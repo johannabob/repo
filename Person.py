33590b5570d06abb6e88a0dfd09154e9475c91f7
Johanna Boberg <johanna.m.boberg@gmail.com>
2021-07-05 21:45:44 +0300

    muutettu Person.py dev2:en

---
+++ Person.py
@@ -0,0 +1,37 @@
+  
+class P2e2r2s2on:
+    def __init__(self, city, address, phone_number):
+        self._city = city
+        self._address = address
+        self._phone_number = phone_number
+
+#muistiinpanosdfgh wertyu er
+#ghjklugfghj fghigj
+
+    @property
+    def city(self, _default = None):
+        return self._city
+
+    @city.setter
+    def city(self, value):
+        self._city = value
+
+    @property
+    def address(self):
+        return self._address
+
+    @address.setter
+    def address(self, value):
+        self._address = value
+
+    @property
+    def phone_number(self, _default=None):
+        return self._phone_number
+
+    @phone_number.setter
+    def phone_number(self, value):
+        self._phone_number = value
+
+    def __str__(self):
+        #muokkailua spotwepotuwpoeti
+        return f'City: {self._city}\nPhone: {self._phone_number}'
