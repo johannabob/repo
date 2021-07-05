96965d1ea6068a464d31d24780ee04787d8000c0
Johanna Boberg <johanna.m.boberg@gmail.com>
2021-07-05 16:00:55 +0300

    add Person.py

---
+++ Person.py
@@ -0,0 +1,12 @@
+class Person:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    #setter
+
+    #getter
+
+    def __str__(self):
+        return "Palautettu Person.py:sta"
